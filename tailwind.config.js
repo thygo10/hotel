0773e4f166ab6a5998de7d6f08c7cf1893a0b282
Thiago Farias <thygo10@icloud.com>
2023-05-18 23:50:16 -0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/bg1.png')",
+    },
   },
   plugins: [],
-};
+}}
