c799934ae7c3f68f81a19e6fc95889b0a2d6973a
Thiago Farias <thygo10@icloud.com>
2023-05-19 22:35:00 -0300

    up

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,10 @@
 import Form from '../Form/Form'
-import bg1 from '../../assets/bg1.png'
+
 
 export default function Hero(){
   return(
     <>
-    <div className="container lg:max-w-5xl max-w-8xl bg- flex  flex-col space-y-3 bg-[url('backgroundImage')] justify-center mx-auto bg-cover h-96 items-center bg-no-repeat">
+    <div className="container lg:max-w-5xl max-w-8xl bg- flex  flex-col space-y-3 bg-[url('src/assets/bg1.png')] justify-center mx-auto bg-cover h-96 items-center bg-no-repeat">
         <div className="text-white text-center mb-10">
           <h2 className="text-4xl text-gray-50">Onde você quer ir?</h2>
           <p className='text-xl py-2'>Escolha o estado e cidade para achar os melhores resorts</p>
